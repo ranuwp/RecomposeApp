743ea04c81dc02e7754bf6eb33d33195c116b72e
Ranu Wijaya Putra <ranu.putra@btpn.com>
2018-07-10 23:53:52 +0700

    [Ranu] added Calculator Higher Order Function

---
+++ App/Enhancers/Calculator.js
@@ -0,0 +1,22 @@
+const multiply = (x) => (y) => {
+  return x * y;
+};
+
+const devide = (x) => (y) => {
+  return x / y;
+};
+
+const add = (x) => (y) => {
+  return x + y;
+};
+
+const substract = (x) => (y) => {
+  return x - y;
+};
+
+export {
+  multiply,
+  devide,
+  add,
+  substract,
+};
