a79b1d0f8a1b7611e8aa513b2d0c8340ba816f8f
Ranu Wijaya Putra <ranu.putra@btpn.com>
2018-07-24 20:15:14 +0700

    [Ranu] downgrade react-native version

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -35,7 +35,9 @@ export default (rootReducer, rootSaga) => {
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
   const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
-  const store = createAppropriateStore(rootReducer, compose(...enhancers))
+//  const store = createAppropriateStore(rootReducer, compose(...enhancers))
+
+  const store = createAppropriateStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), compose(...enhancers));
 
   // configure persistStore and check reducer version number
   if (ReduxPersist.active) {
